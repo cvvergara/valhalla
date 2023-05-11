52f68b82efb5717ca9ec6e275e2a841a4ff26eae
cvvergara <vicky@georepublic.de>
2023-05-11 13:06:04 -0600

    [test/test.cc] Using std::filesystem

--- test/test.cc
+++ test/test.cc
@@ -23,7 +23,7 @@
 
 #include <boost/algorithm/string.hpp>
 
-#include "filesystem.h"
+#include <filesystem>
 #include "microtar.h"
 
 namespace {
@@ -441,8 +441,8 @@ void build_live_traffic_data(const boost::property_tree::ptree& config,
   std::string tile_dir = config.get<std::string>("mjolnir.tile_dir");
   std::string traffic_extract = config.get<std::string>("mjolnir.traffic_extract");
 
-  filesystem::path parent_dir = filesystem::path(traffic_extract).parent_path();
-  if (!filesystem::exists(parent_dir)) {
+  std::filesystem::path parent_dir = std::filesystem::path(traffic_extract).parent_path();
+  if (!std::filesystem::exists(parent_dir)) {
     std::stringstream ss;
     ss << "Traffic extract directory " << parent_dir.string() << " does not exist";
     throw std::runtime_error(ss.str());
