b3b83be8343298117cab4c17416a4d254fd1588e
cvvergara <vicky@georepublic.de>
2023-05-11 13:06:04 -0600

    [test/http_tiles.cc] Using std::filesystem

--- test/http_tiles.cc
+++ test/http_tiles.cc
@@ -3,7 +3,7 @@
 #include "baldr/curl_tilegetter.h"
 #include "baldr/graphtile.h"
 #include "tyr/actor.h"
-#include "valhalla/filesystem.h"
+#include <filesystem>
 #include "valhalla/tile_server.h"
 
 #include <prime_server/prime_server.hpp>
@@ -71,10 +71,10 @@ TEST(HttpTiles, test_no_cache_gz) {
 class HttpTilesWithCache : public ::testing::Test {
 protected:
   void SetUp() override {
-    filesystem::remove_all("url_tile_cache");
+    std::filesystem::remove_all("url_tile_cache");
   }
   void TearDown() override {
-    filesystem::remove_all("url_tile_cache");
+    std::filesystem::remove_all("url_tile_cache");
   }
 };
 
