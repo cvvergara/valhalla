c3bb258a792df1f56d20ec417e6a6d236799b0a4
cvvergara <vicky@georepublic.de>
2023-05-11 13:06:04 -0600

    [src/mjolnir/statistics.cc] Using std::filesystem

--- src/mjolnir/statistics.cc
+++ src/mjolnir/statistics.cc
@@ -1,6 +1,6 @@
 #include <cstdint>
 
-#include "filesystem.h"
+#include <filesystem>
 #include "mjolnir/graphvalidator.h"
 #include "statistics.h"
 
@@ -516,8 +516,8 @@ void statistics::RouletteData::GenerateTasks(const boost::property_tree::ptree& 
                     std::string("This node is either unreachable or unleavable. Edit the surrounding "
                                 "roads so that the node can be accessed properly")}})}})}});
   // write out to a file
-  if (filesystem::exists("maproulette_tasks.geojson")) {
-    filesystem::remove("maproulette_tasks.geojson");
+  if (std::filesystem::exists("maproulette_tasks.geojson")) {
+    std::filesystem::remove("maproulette_tasks.geojson");
   }
   std::ofstream file;
   file.open("maproulette_tasks.geojson");
